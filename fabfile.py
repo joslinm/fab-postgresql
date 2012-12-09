22cb922106a64284460db520d013a1792b53b903
Mark Joslin <mjoslin@gmail.com>
2012-12-09 00:19:18 +0000

    Get fab working with sudo

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,18 @@
+from fabric.api import run, sudo
+from fabric.context_managers import cd
+from fabric.operations import sudo
+from fabric.api import run, env
+import os
+
+HOME = os.getenv('HOME')
+
+env.user = 'ec2-user'
+env.key_filename = [
+    '%s/key.pem'%HOME
+] #assuming key
+
 def hello():
-  print "HI!"
+  print "HI!";
 
-
+def install(config='standard'):
+  sudo('echo hi');
